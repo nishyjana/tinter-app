e21f0d9d08ecc67a3636dab1d23edb652878ef6f
nishy <nishyjana6435@gmail.com>
2021-01-08 02:55:21 +0530

    ui done

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Header from './Header';
+import TinderCards from './Tindercards';
+import SwipeButtons from './SwipeButtons';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Chats from './Chats';
+import ChatScreen from './ChatScreen';
+
+
+
+function App() {
+  return (
+    //code
+    <div className="App">
+   
+  <Router>
+   <Switch>
+   <Route path="/chat/:person">
+        <Header backButton="/chat" />
+            
+             <ChatScreen/>
+    </Route>
+    <Route path="/chat">
+        <Header backButton="/" />
+             <Chats/>
+    </Route>
+    <Route path="/">
+        <Header />
+        <TinderCards/>
+        <SwipeButtons/>
+    </Route>
+   </Switch>
+  </Router>
+   
+   
+    
+    </div>
+  );
+}
+
+export default App;
